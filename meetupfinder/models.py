7db2ed6a23547c01f3daf07703942e9c70d3b2c5
Danielle Zevitz <dezevitz@gmail.com>
2020-10-21 16:30:29 -0400

    new tables in graphs

--- meetupfinder/models.py
+++ meetupfinder/models.py
@@ -3,7 +3,12 @@ from django.utils import timezone
 
 # Create your models here.
 class Event(models.Model):
-   event_text= models.CharField(max_length=200)
-   event_date= models.DateTimeField('event date')
-   def __str__(self):
+    event_name= models.CharField(max_length=200)
+    event_text= models.CharField(max_length=400)
+    event_host= models.CharField(max_length=200)
+    event_date= models.DateTimeField('event date')
+    end_event_date= models.DateTimeField('end event date')
+    def __str__(self):
        return self.event_text
+# location -- text field or something else
+# categories (own model)
