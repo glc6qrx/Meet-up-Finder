2514aa10c4cb47d56421148d278e0c795507f0a2
snyshadham12 <sn5hnj@virginia.edu>
2020-11-05 23:36:48 -0500

    Category filter added

--- meetupfinder/forms.py
+++ meetupfinder/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from .models import Categories
 
 class EventFilterForm(forms.Form):
     name = forms.CharField(label='Event Name', max_length=200, required=False,
@@ -9,3 +10,6 @@ class EventFilterForm(forms.Form):
 
     end_date = forms.DateField(label='End Date', required=False, input_formats={'%m/%d/%y'},
         widget=forms.DateInput(attrs={'class': 'form-control', 'placeholder': 'MM/DD/YY'}))
+        
+    category = forms.ModelMultipleChoiceField(label="Categories (SHIFT-Click for multiple)", required=False,
+    queryset=Categories.objects.all(), widget=forms.SelectMultiple(attrs={'class': 'form-control'}))
